2f4c340025d8e579ee1802135636e703957f1bf3
root <edu_vcd@hotmail.com>
2016-12-14 17:22:30 -0200

    x

--- set_environment/startflask.sh
+++ set_environment/startflask.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FPATH='/path/to/flask/'
-VPATH='/path/to/venv/bin'
+FPATH='/home/lollip14/tmp/gis/flask'
+VPATH='/home/lollip14/tmp/venv/bin/activate'
 
 CFP=$(echo $FPATH | sed 's/$/\//' | sed 's/\/\//\//')
 CVP=$(echo $VPATH | sed 's/$/\//' | sed 's/\/\//\//')
@@ -10,6 +10,7 @@ if [ ! "$echo $?" -eq 0 ]
 then
     echo "The program wasn't able to start the virtual environment."
     echo "Check if the VPATH variable is set to the correct file path."
+    exit 1
 fi
 export FLASK_DEBUG=1
 export FLASK_APP=$CFP\app.py
